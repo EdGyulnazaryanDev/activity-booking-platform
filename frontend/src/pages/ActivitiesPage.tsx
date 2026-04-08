40490abda04b1ae58603667815dc20f5264f4536
Ed Gyulnazaryan <edik.gyulnazaryan@smartsoft.am>
2026-04-09 00:49:32 +0400

    fix: remove unused useI18n import

--- frontend/src/pages/ActivitiesPage.tsx
+++ frontend/src/pages/ActivitiesPage.tsx
@@ -1,4 +1,3 @@
-import { useI18n } from '../contexts/I18nContext';
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { resourcesAPI } from '../services/api';
